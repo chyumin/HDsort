714e3ecda694de0d136b01b62ccc6694b769d23e
rolandd <roland.diggelmann@bsse.ethz.ch>
2018-06-19 15:22:18 +0200

    Change in function

--- +hdsort/+waveforms/tShift.m
+++ +hdsort/+waveforms/tShift.m
@@ -1,14 +1,24 @@
 function T = tShift(T, tau, trunc)
-    % shifts the single channel waveforms stored in T
-    % truncates in a way, that A has the same dimensions as T
-    % If a waveform has a shift of 0, and trunc is 1 it will be unchanged.
-    
-    if nargin < 3; trunc = 0; end
-    tau = tau(:);
-    assert(~any(tau~=round(tau)), 'tau must contain only integers!');
-    nC = size(T,2);
+% shifts the single channel waveforms stored in T
+% truncates in a way, that A has the same dimensions as T
+% If a waveform has a shift of 0, and trunc is 1 it will be unchanged.
+
+if nargin < 3; trunc = 0; end
+tau = tau(:);
+assert(~any(tau~=round(tau)), 'tau must contain only integers!');
+
+[nTf, nC, nU] = size(T);
+
+if size(tau, 1) == 1
+    tau = repmat(tau', nC*nU, 1);
+else
+    assert(size(tau, 1) == nU, 'You must provide one tau for each unit!')
     tau = repmat(tau', nC, 1);
-    tau = tau(:);    
-    T = hdsort.waveforms.t2m(T);
-    T = hdsort.util.shiftRows(T, tau, trunc);
-    T = hdsort.waveforms.m2t(T, nC);   
+end
+tau = tau(:);
+
+T = hdsort.waveforms.t2m(T);
+T = hdsort.util.shiftRows(T, tau, trunc);
+T = hdsort.waveforms.m2t(T, nC);
+
+end
