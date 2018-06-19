6b681063d3b6291eca7404078dd344077b27fc36
rolandd <roland.diggelmann@bsse.ethz.ch>
2018-06-19 16:28:45 +0200

    Several files removed

--- +hdsort/sorting_example.m
+++ +hdsort/sorting_example.m
@@ -2,37 +2,61 @@
 %% New test scenario:
 pd = hdsort.pathDefinitions()
 
-rawFile = fullfile( pd.mea1kRoot, 'rolandd', '160513', 'data', 'Trace_20160513_12_07_48.raw.h5')
-configFile = fullfile( pd.mea1kRoot, 'rolandd', '160513', 'config', '11_30_06.mapping.nrk')
-preprocessedFolder = fullfile(pd.mea1kIntermediate, 'rolandd', '161220');
-sortingName = 'testGrid_161220'
-
-%%
-preprocessjob = hdsort.grid.PreprocessJob(sortingName, preprocessedFolder, {rawFile}, 'configFile', configFile);
-if ~preprocessjob.alreadyPreprocessed()
-    preprocessjob.setTaskParameters();
-    preprocessjob.createAutoSubmitToken();
-    all_tasks_completed = preprocessjob.waitForTasksToFinish(10);
+if 0
+    rawFile = fullfile( pd.mea1kRoot, 'rolandd', '160513', 'data', 'Trace_20160513_12_07_48.raw.h5')
+    configFile = fullfile( pd.mea1kRoot, 'rolandd', '160513', 'config', '11_30_06.mapping.nrk')
+    mainFolder = fullfile(pd.mea1kIntermediate, 'rolandd', '161220');
+    sortingName = 'testGrid_161220'
+    preprocessjob = hdsort.grid.PreprocessJob(sortingName, mainFolder, {rawFile}, 'configFile', configFile);
+    
+    if ~preprocessjob.alreadyPreprocessed()
+        preprocessjob.setTaskParameters();
+        preprocessjob.createAutoSubmitToken();
+        all_tasks_completed = preprocessjob.waitForTasksToFinish(10);
+    end
+    
+    [preprocessedFiles, rawFiles, cmdFiles] = preprocessjob.getFileNames();
+    %%
+else
+    rawFile = fullfile('./Tutorial', 'new_file02.raw.h5')
+    sortingName = 'tutorial_sorting';
+    %mainFolder = fullfile(pd.mea1kIntermediate, 'rolandd', 'hd_tutorial');
+    mainFolder = fullfile('./Tutorial')
+    mkdir(mainFolder);
+    RAW = hdsort.file.BELMEAFile(rawFile);
+    preprocessedFiles = {RAW.preprocessFile(mainFolder)};
 end
 
-[preprocessedFiles, rawFiles, cmdFiles] = preprocessjob.getFileNames();
 %%
+DS = hdsort.file.CMOSMEA(preprocessedFiles);
 
-DS = hdsort.filewrapper.CMOSMEA(preprocessedFiles);
-sorting = hdsort.Sorting(DS, preprocessedFolder, sortingName)
 %%
-sorting.startSorting('sortingMode', 'QSUB')
-%%
-sorting.startSorting('sortingMode', 'grid_for')
-%%
-sorting.postprocessGridSorting()
+resultFile = fullfile(mainFolder, 'results.mat');
 
-
-%% Alternate:
-[gdf_merged, T_merged, localSorting, localSortingID, sessionLengths] = hdsort.startHDSorting(preprocessedFiles, preprocessedFolder, 'testGrid_startHDSorting')
+if exist(resultFile)
+    load(resultFile)
+else
+    if 0
+        sorting = hdsort.Sorting(DS, mainFolder, sortingName)
+        %%
+        [R, P] = sorting.startSorting('sortingMode', 'QSUB')
+        %%
+        [R, P] = sorting.startSorting('sortingMode', 'grid_for')
+        
+        %%
+        [R, P] = sorting.startSorting('sortingMode', 'localHDSorting')
+        
+        %%
+        [R, P] = sorting.postprocessGridSorting()
+    else
+        %% Alternate:
+        [R, P] = hdsort.startHDSorting(DS, mainFolder, sortingName)
+    end
+    save(resultFile, 'R', 'P')
+end
 
 % -------------------------------------------------------------------------
-%% Test hdsort.filewrapper.CortexLabFiles
+%% Test hdsort.file.CortexLabFiles
 folderSet = '/Volumes/hierlemann/intermediate_data/Mea1k/rolandd/cortexlab/set1';
 datFile_ = dir([folderSet '/*.dat']);
 datFile = fullfile(folderSet, datFile_.name);
@@ -40,28 +64,28 @@ datFile = fullfile(folderSet, datFile_.name);
 prmFile_ = dir([folderSet '/*.prm']);
 prmFile = fullfile(folderSet, prmFile_.name);
 %
-M = hdsort.filewrapper.CortexLabFile(datFile, prmFile)
+M = hdsort.file.CortexLabFile(datFile, prmFile)
 %%
 preM = M.preprocessFile('/Users/rolandd/tmp')
 
 % -------------------------------------------------------------------------
-%% Test hdsort.filewrapper.CMOSMEA
+%% Test hdsort.file.CMOSMEA
 folder = '/Volumes/hierlemann/intermediate_data/Mea1k/rolandd/160513/preprocessed';
 fileList = {[folder '/Trace_20160513_11_32_04.h5'], [folder '/Trace_20160513_11_34_21.h5']}
-preprocessedFolder = folder;
+mainFolder = folder;
 
 %%
-%DS = hdsort.filewrapper.CMOSMEA(fileList{1});
+%DS = hdsort.file.CMOSMEA(fileList{1});
 %covest_old = DS.getCovest();
 
 
-%% Test single file hdsort.filewrapper.CMOSMEAFile
-%cmosmeafile = hdsort.filewrapper.CMOSMEAFile(fileList{1});
+%% Test single file hdsort.file.CMOSMEAFile
+%cmosmeafile = hdsort.file.CMOSMEAFile(fileList{1});
 %clist = cmosmeafile.getChannelList();
 %covest = cmosmeafile.getCovest();
 
-%% Test multifile hdsort.filewrapper.CMOSMEA
-cmoslist = hdsort.filewrapper.CMOSMEA(fileList);
+%% Test multifile hdsort.file.CMOSMEA
+cmoslist = hdsort.file.CMOSMEA(fileList);
 %covest_list = cmoslist.getCovest();
 
 %%
@@ -91,8 +115,8 @@ stold = DSList.detectSpikes('thr', 5);
 stnew = cmoslist.detectSpikes('thr', 5);
 
 %%
-cutLeft = 20; 
-cutLength = 50; 
+cutLeft = 20;
+cutLength = 50;
 channels = 1:10;
 
 wfold = DSList.getWaveform(stold{1}, cutLeft, cutLength, channels);
@@ -121,7 +145,7 @@ figure; plot(dc); hold on; plot(dc_old+1000)
 
 %%
 M = randn(10000, 100);
-MM = hdsort.filewrapper.DataMatrix(M);
+MM = hdsort.file.DataMatrix(M);
 
 %%
 MM.restrictToChannels(1:2);
